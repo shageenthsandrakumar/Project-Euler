56c2e2c828a7cc152b718100e65d42400fc06362
Shageenth Sandrakumar <shageenthsandrakumar@gmail.com>
2025-10-11 18:42:28 -0400

    Add solution 1 for Problem 1

---
+++ Problem1_solution_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+number_of_3s = int(1000/3)
+number_of_5s = 1000/5-1
+number_of_15s = int(1000/15)
+
+sum_of_3s = 3*(number_of_3s)*(number_of_3s+1)/2
+sum_of_5s = 5*(number_of_5s)*(number_of_5s+1)/2
+sum_of_15s = 15*(number_of_15s)*(number_of_15s+1)/2
+
+final_sum = sum_of_3s + sum_of_5s - sum_of_15s
+print(int(final_sum))
+
