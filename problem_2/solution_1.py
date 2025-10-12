a112fdcc3c0352d4cb3b72dc6adefad9cf382bb0
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-12 10:56:43 -0400

    Create solution_1.py

---
+++ problem_2/solution_1.py
@@ -0,0 +1,10 @@
+previous = 0
+current = 1
+sum = 0 
+threshold = 4000000
+while current < threshold:
+    previous,current = current,previous+current
+    if not previous%2:
+        sum += previous
+
+print(sum)
