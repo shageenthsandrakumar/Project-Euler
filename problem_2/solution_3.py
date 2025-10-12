716ba2281868b5bda5181ccdb7a9d5674241ddea
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-12 11:33:27 -0400

    Create solution_3.py

---
+++ problem_2/solution_3.py
@@ -0,0 +1,28 @@
+import math
+
+# constants
+phi = (1 + math.sqrt(5)) / 2
+psi = (1 - math.sqrt(5)) / 2
+
+# powers for even fibinacci terms
+ephi = phi**3
+epsi = psi**3
+N = 0
+E_N = 0
+while E_N < 4000000:
+    N += 1
+    E_N = (ephi**N - epsi**N) / sqrt5
+#N represents index of first even fibinacci number over threshold.
+#Use one less to represent indicies of numbers below threshold. 
+N -= 1
+
+# geometric series sums
+sum_ephi = ephi * (ephi**N - 1) / (ephi - 1)
+sum_epsi = epsi * (epsi**N - 1) / (epsi - 1)
+
+# total sum of even Fibonacci numbers
+sum_even = (sum_ephi - sum_epsi) / sqrt5
+
+# round to nearest integer for exact sum
+sum_even = round(sum_even)
+print(sum_even)
