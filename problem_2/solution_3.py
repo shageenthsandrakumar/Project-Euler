4d60e66c35a6a5ca5da37546710ac8fb9f8ae834
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-13 16:54:18 -0400

    Update solution_3.py

--- problem_2/solution_3.py
+++ problem_2/solution_3.py
@@ -1,10 +1,8 @@
 previous = 0
-current = 1
+current = 2
 sum = 0 
 threshold = 4000000
 while current < threshold:
-    previous,current = current,previous+current
-    if not previous%2:
-        sum += previous
-
+    previous,current = current,previous+4*current
+    sum += previous
 print(sum)
