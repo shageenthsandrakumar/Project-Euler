74478640bf182f3993040aeaf80047c8533e0de6
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-13 20:34:43 -0400

    Create solution_3.py

---
+++ problem_3/solution_3.py
@@ -0,0 +1,20 @@
+n = 600851475143
+max_factor = 0
+while not n%2:
+    max_factor = 2 
+    n //= 2
+
+while not n%3:
+    max_factor = 3
+    n //= 3
+f = 5
+toggle = False
+while f*f <= n:
+    while not n%f:
+        max_factor = f
+        n //= f
+    f += 2+2*int(toggle)
+    toggle = not toggle
+if n > 1:
+    max_factor = n
+print(max_factor)
