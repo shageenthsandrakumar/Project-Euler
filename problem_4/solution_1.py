d18cea8def3346b061f2eaa275e569018def8e5f
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-17 23:40:52 -0400

    Update solution_1.py

--- problem_4/solution_1.py
+++ problem_4/solution_1.py
@@ -6,14 +6,13 @@ def ispalindrome(n):
          return False
     return True 
 
-palindromes = []
+largest = (0,0,0)
 for i in range(100,1000):
     for j in range(i,1000):
         product = i*j
         if ispalindrome(product):
-            palindromes.append((product,i,j))
-palindromes.sort(reverse=True)
-
-answer,a,b = palindromes[0]
+            if product > largest[0]:
+                largest = (product,i,j)
+answer,a,b = largest
 print(f"The largest palindrome product of two 3-digit numbers is: {answer}")
 print(f"It is the product of {a} × {b}")
