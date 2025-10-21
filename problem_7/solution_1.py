a1fe1c409343125b9c2f51300d0f514e6f080676
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-21 16:51:14 -0400

    Create solution_1.py

---
+++ problem_7/solution_1.py
@@ -0,0 +1,9 @@
+threshold = 10001
+primes = []
+n = 1
+while len(primes) < threshold:
+    n += 1
+    if not any(n % p == 0 for p in primes):
+        primes.append(n)
+nth_prime = n
+print(nth_prime)
