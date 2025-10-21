a3f57c81d8966ca4ab83df171616c8fe72209322
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-20 23:32:11 -0400

    Update solution_2.py

--- problem_5/solution_2.py
+++ problem_5/solution_2.py
@@ -1,6 +1,6 @@
 import numpy as np
 nums = 20
-is_prime = np.ones((nums//2,), dtype=bool) # index i = 2*i + 1 
+is_prime = np.ones(((nums+1)//2,), dtype=bool) # index i = 2*i + 1
 is_prime[0] = False 
 for i in range(3, int(np.sqrt(nums)) + 1, 2): 
     if is_prime[i//2]:
