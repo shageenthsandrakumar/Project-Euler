bb661aa2bcfaf364b59a7e5258b9d9685be2ef7f
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-20 22:04:30 -0400

    Update solution_1.py

--- problem_5/solution_1.py
+++ problem_5/solution_1.py
@@ -1,10 +1,9 @@
 import numpy as np
 nums = 20
 primes = [2] 
-if nums > 2:
-    for n in range(3, nums + 1):
-        if not any(n % p == 0 for p in primes):
-            primes.append(n)
+for n in range(3, nums + 1):
+    if not any(n % p == 0 for p in primes):
+        primes.append(n)
 primes = np.array(primes) 
 exponents = np.log(nums)//np.log(primes)
 answer = int(np.prod(primes**exponents))
