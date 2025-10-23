4547522e2ec65df07c215f940f7628f394542cf2
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-23 08:55:48 -0400

    Update solution_1.py

--- problem_010/solution_1.py
+++ problem_010/solution_1.py
@@ -1,9 +1,9 @@
 limit = 2000000
-is_prime = np.ones(((limit+1)//2,), dtype=bool) # index i = 2*i + 1
+is_prime = np.ones(((limit-1)//2,), dtype=bool) # index i = 2*i + 1
 is_prime[0] = False 
 for i in range(3, int(np.sqrt(limit)) + 1, 2): 
     if is_prime[i//2]:
         is_prime[i*i//2::i] = False 
 primes = np.r_[2, 2*np.nonzero(is_prime)[0]+1]
-answer = sum(primes))
+answer = sum(primes)
 print(answer)
