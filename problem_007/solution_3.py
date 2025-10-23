273ddde7778071dbcf0185f79317ae25f706df2f
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-22 22:33:03 -0400

    Create solution_3.py

---
+++ problem_007/solution_3.py
@@ -0,0 +1,24 @@
+import itertools
+nth = 10001
+primes_found = 0
+last_prime = 0
+def prime_generator():
+    yield 2  # Start with 2
+    D = {}
+    for c in itertools.count(3, 2):
+        if c not in D:
+            D[c * c] = 2 * c
+            yield c
+        else:
+            step = D.pop(c)
+            next_multiple = c + step
+            while next_multiple in D:
+                next_multiple += step
+            D[next_multiple] = step
+prime_gen = prime_generator()
+for p in prime_gen:
+    primes_found += 1
+    last_prime = p
+    if primes_found == nth:
+        break
+print(last_prime)
