3aac64a84320ed52233bb7c9190484479b0658eb
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-24 11:50:00 -0400

    Create solution_3.py

---
+++ problem_005/solution_3.py
@@ -0,0 +1,24 @@
+import itertools
+import math 
+nums = 20
+answer = 1
+def prime_generator():
+    yield 2  # Start with 2
+    D = {}
+    for c in itertools.count(3, 2):
+        if c not in D:
+            D[c * c] = 2 * c
+            yield c
+        else:
+            step = D.pop(c)
+            next_multiple = c + step
+            while next_multiple in D:
+                next_multiple += step
+            D[next_multiple] = step
+prime_gen = prime_generator()
+for p in prime_gen:
+    if p > nums:
+        break
+    else:
+        answer *= int(p**(math.log(nums)//math.log(p)))
+print(answer)
