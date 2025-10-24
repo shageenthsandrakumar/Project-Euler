c2809ac2d8dc3ef4756827b3419708cb7c580216
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-24 11:28:59 -0400

    Create solution_4.py

---
+++ problem_010/solution_4.py
@@ -0,0 +1,23 @@
+import itertools
+threshold = 2000000
+prime_sum = 0
+def prime_generator():
+    yield 2  # Start with 2
+    D = {}
+    for c in itertools.count(3, 2):
+        if c not in D:
+            D[c * c] = 2 * c
+            yield c
+        else:
+            step = D.pop(c)
+            next_multiple = c + step
+            while next_multiple in D:
+                next_multiple += step
+            D[next_multiple] = step
+prime_gen = prime_generator()
+for p in prime_gen:
+    if p >= threshold:
+        break
+    else:
+        prime_sum += p
+print(prime_sum)
