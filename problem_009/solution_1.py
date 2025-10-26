da7cccc0fcd008388bf4a4ef2f5cb083b1b5b928
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-25 21:47:23 -0400

    Update solution_1.py

--- problem_009/solution_1.py
+++ problem_009/solution_1.py
@@ -1,15 +1,18 @@
 import math
-P = 1000
+P = 1001
 triplets = []
-for a in range(1,(P-1)//3+1):
-    numerator = P * (P - 2 * a)
-    denominator = 2 * (P - a)
-    if not numerator % denominator:
-        b = numerator//denominator
-        c = P - a - b
-        if a < b:
-            if a**2 + b**2 == c**2:
-                triplets.append((a, b, c))
+if P % 2 != 0:
+    print(f"Perimeter {P} is not an even integer, no integer Pythagorean Triples can exist.")
+else:
+    for a in range(1,(P-1)//3+1):
+        numerator = P * (P - 2 * a)
+        denominator = 2 * (P - a)
+        if not numerator % denominator:
+            b = numerator//denominator
+            c = P - a - b
+            if a < b:
+                if a**2 + b**2 == c**2:
+                    triplets.append((a, b, c))
 if triplets:
     print(f"Found {len(triplets)} Pythagorean Triplet(s) where a + b + c = {P}:")
     for a, b, c in triplets:
