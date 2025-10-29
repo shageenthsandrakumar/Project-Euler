cde49312cdf927f18ba4a278c675eb9218f554ec
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-28 20:46:09 -0400

    Update solution_2.py

--- problem_012/solution_2.py
+++ problem_012/solution_2.py
@@ -1,17 +1,16 @@
 threshold = 500
 def number_of_divisors(n):
     exponents_plus1 = []
-    count = 0
+    power_2 = 0
     while not n%2:
         n //= 2
-        count += 1
-    exponents_plus1.append(count+1)
-    power_2 = count
-    count = 0
+        power_2 += 1
+    exponents_plus1.append(power_2+1)
+    power_3 = 0
     while not n%3:
         n //= 3
-        count += 1
-    exponents_plus1.append(count+1)
+        power_3 += 1
+    exponents_plus1.append(power_3+1)
     f = 5
     step = 2
     while f*f <= n:
@@ -29,9 +28,9 @@ n = 0
 divisor_count = 0
 while divisor_count <= threshold:
     n += 1
-    tau_n,power_n = number_of_divisors(n)
-    tau_n1,power_n1 = number_of_divisors(n+1)
-    power_even = power_n+power_n1
+    tau_n,power2_n = number_of_divisors(n)
+    tau_n1,power2_n1 = number_of_divisors(n+1)
+    power_even = power2_n+power2_n1
     divisor_count = tau_n*tau_n1*power_even/(power_even + 1)
 answer = n*(n+1)//2
 print(answer)
