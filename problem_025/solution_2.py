d1686f289f5e6d21fc9a08858a3de37247f289e4
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-10-29 16:47:36 -0400

    Update solution_2.py

--- problem_025/solution_2.py
+++ problem_025/solution_2.py
@@ -4,7 +4,7 @@ previous = 0
 current = 1
 threshold = digits-1
 n = 1
-while math.log10(current) <= threshold:
+while math.log10(current) < threshold:
     previous,current = current,previous+current
     n += 1
 print(n)
