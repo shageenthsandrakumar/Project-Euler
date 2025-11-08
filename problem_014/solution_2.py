d715b00694f041b1a353ee2e93fe8f343f12b00e
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-11-07 19:37:14 -0500

    Update solution_2.py

--- problem_014/solution_2.py
+++ problem_014/solution_2.py
@@ -4,15 +4,16 @@ collatz_lengths[1] = 1
 def collatz_length(n):
     if n in collatz_lengths:
         return collatz_lengths[n]
-    elif n%2:
-        return 1+collatz_length(3*n+1)
-    else:
-        return 1+collatz_length(n//2)
+    nxt,is_odd = divmod(n,2)
+    nxt += is_odd*(5*nxt+4)
+    L = 1+collatz_length(nxt)
+    collatz_lengths[n] = L
+    return L
+
 max_length = 0
 start_number = 0
-for i in range(1,1000000):
+for i in range(1000000-1,1,-1):
     c = collatz_length(i)
-    collatz_lengths[i] = c
     if max_length < c:
         max_length = c
         start_number = i
