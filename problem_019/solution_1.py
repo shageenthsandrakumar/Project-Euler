19bd698ab09b6d401891d9b69dbe2209cd7212bd
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-11-16 14:27:08 -0500

    Update solution_1.py

--- problem_019/solution_1.py
+++ problem_019/solution_1.py
@@ -7,7 +7,6 @@ def daterange(start_date, end_date):
 start_dt = date(1901, 1, 1)
 end_dt = date(2000, 12, 31)
 count = 0
-print(f"Dates between {start_dt} and {end_dt}:")
 for dt in daterange(start_dt, end_dt):
     if not (dt.day-1+(dt.weekday()+1)%7):
         count += 1
