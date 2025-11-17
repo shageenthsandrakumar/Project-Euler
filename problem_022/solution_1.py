b44cd03116ec087a7a6bb85e3f0c27e9faa0593e
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-11-17 18:31:43 -0500

    Update solution_1.py

--- problem_022/solution_1.py
+++ problem_022/solution_1.py
@@ -2,4 +2,5 @@ with open("0022_names.txt", "r") as file:
     text = file.read()
 names = [name.replace('"', '') for name in text.split(",")]
 names.sort()
-print(sum((i+1)*sum(ord(n.lower()) - 96 for n in names[i]) for i in range(len(names))))
+total_score = sum((i+1)*sum(ord(n.lower()) - 96 for n in names[i]) for i in range(len(names)))
+print(total_score)
