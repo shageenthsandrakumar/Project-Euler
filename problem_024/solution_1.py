4fe59d07bc3824cc3d7ce98bf302e704acc2ad66
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-11-17 21:51:15 -0500

    Update solution_1.py

--- problem_024/solution_1.py
+++ problem_024/solution_1.py
@@ -1,14 +1,7 @@
-import math 
+from itertools import permutations
 nth = 10**6
 highest_number = 9
 numbers = list(range(highest_number+1))
-combination = ""
-number = nth-1
-n = len(numbers)-1
-while numbers and number:
-    index,number = divmod(number,math.factorial(n))
-    combination += str(numbers.pop(index))
-    n -= 1
-for num in numbers:
-    combination += str(num)
+sorted_list = list(permutations(numbers))
+combination = "".join(map(str, sorted_list[nth-1]))
 print(combination)
