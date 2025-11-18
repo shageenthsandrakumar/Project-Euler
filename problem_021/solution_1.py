469f0889b0d0a4a0d56ea241cc25a79f3d613712
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-11-18 14:58:03 -0500

    Create solution_1.py

---
+++ problem_021/solution_1.py
@@ -0,0 +1,37 @@
+def d(n):
+    orginal_n = n
+    power_2 = 0
+    while not n%2:
+        n //= 2
+        power_2 += 1
+    answer = 2**(power_2+1)-1
+    power_3 = 0
+    while not n%3:
+        n //= 3
+        power_3 += 1
+    answer *= (3**(power_3+1)-1)//2
+    f = 5
+    step = 2
+    while f*f <= n:
+        count = 0 
+        while not n%f:
+            n //= f
+            count += 1
+        if count:
+            answer *= (f**(count+1)-1)//(f-1)
+        f += step
+        step = 6-step
+    if n > 1:
+        answer *= n+1
+    return answer-orginal_n
+
+amicable_sum = 0
+d_values = {}
+for a in range(1,10000):
+    d_value = d(a)
+    if d_value in d_values:
+        if d_values[d_value] == a and d_value != a:
+            amicable_sum += d_value+a
+    else:
+        d_values[a] = d_value
+print(amicable_sum)
