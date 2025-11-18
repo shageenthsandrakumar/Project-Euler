a2bc8ba0b4e233e598014751b74638e10d53d0ef
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-11-18 15:37:22 -0500

    Create solution_2.py

---
+++ problem_021/solution_2.py
@@ -0,0 +1,37 @@
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def d(n):
+    if not n:
+        return n
+    orginal_n = n
+    power_2 = 0
+    while not n%2:
+        n //= 2
+        power_2 += 1
+    answer = 2**(power_2+1)-1
+    power_3 = 0
+    while not n%3:
+        n //= 3
+        power_3 += 1
+    answer *= (3**(power_3+1)-1)//2
+    f = 5
+    step = 2
+    while f*f <= n:
+        count = 0 
+        while not n%f:
+            n //= f
+            count += 1
+        if count:
+            answer *= (f**(count+1)-1)//(f-1)
+        f += step
+        step = 6-step
+    if n > 1:
+        answer *= n+1
+    return answer-orginal_n
+amicable_sum = 0
+threshold = 10000
+for a in range(threshold):
+    d_value = d(a)
+    if d_value < a:
+        amicable_sum += (d_value+a)*int(d(d_value) == a)
+print(amicable_sum)
