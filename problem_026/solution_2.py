b40e1d7291f7b18d1ad1df8bf5a23df45fd9ab30
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-12-20 17:52:01 -0500

    Update solution_2.py

--- problem_026/solution_2.py
+++ problem_026/solution_2.py
@@ -1,36 +1,23 @@
-import numpy as np
-threshold = 1000
-is_prime = np.ones(((threshold+1)//2,), dtype=bool) # index i = 2*i + 1
-is_prime[0] = False 
-for i in range(3, int(np.sqrt(threshold)) + 1, 2): 
-    if is_prime[i//2]:
-        is_prime[i*i//2::i] = False 
-primes = np.r_[2, 2*np.nonzero(is_prime)[0]+1]
+def cycle_length(d):
+    if not d:
+        return 0
+    while not d%2:
+        d //= 2
+    while not d%5:
+        d //= 5
+    if not d-1:
+        return 0
+    power = 1
+    while pow(10,power,d)-1:  # (10^power) mod d != 1 
+        power += 1
+    return power
 
-def is_full_reptend(p):
-    n = p-1
-    prime_factors = []
-    index = 0
-    f = primes[index]
-    while f*f <= n:
-        is_factor = False 
-        while not n%f:
-            n //= f
-            is_factor = True
-        if is_factor:
-            prime_factors.append(f)
-        index += 1
-        f = primes[index]
-    if n > 1:
-        prime_factors.append(n)
-    for q in prime_factors:
-        exp = int((p - 1)/q) 
-        if pow(10, exp, p) == 1:
-            return False
-    return True
+threshold = 1000
 d_max = 0
-for prime in reversed(primes):
-    if is_full_reptend(int(prime)):
-        d_max = prime
-        break
+max_length = -1
+for d in range(threshold):
+    length = cycle_length(d)
+    if length > max_length:
+        max_length =length
+        d_max = d
 print(d_max)
