5b9e0f67a9bb323de03373b48a8f07f80a0b5dec
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2025-12-29 12:21:31 -0500

    Refactor number_of_divisors function for efficiency

--- problem_012/solution_2.py
+++ problem_012/solution_2.py
@@ -1,17 +1,16 @@
 import math
 threshold = 500
 def number_of_divisors(n):
-    exponents_plus1 = []
     power_2 = 0
     while not n%2:
         n //= 2
         power_2 += 1
-    exponents_plus1.append(power_2+1)
+    answer = power_2+1
     power_3 = 0
     while not n%3:
         n //= 3
         power_3 += 1
-    exponents_plus1.append(power_3+1)
+    answer *= power_3+1
     f = 5
     step = 2
     while f*f <= n:
@@ -21,10 +20,10 @@ def number_of_divisors(n):
             count += 1
         f += step
         step = 6-step
-        exponents_plus1.append(count+1)
+        answer *= count+1
     if n > 1:
-        exponents_plus1.append(2)
-    return math.prod(exponents_plus1),power_2
+        answer *=2
+    return answer,power_2
 n = 0
 divisor_count = 0
 while divisor_count <= threshold:
