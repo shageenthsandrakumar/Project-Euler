eb2ac2da1da6be98fd73df7d3e8a3d525f2e34fb
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2026-03-03 13:14:11 -0500

    Update solution_2.py

--- problem_029/solution_2.py
+++ problem_029/solution_2.py
@@ -1,4 +1,6 @@
 from math import log2
+lower_bound = 2
+upper_bound = 100
 
 def get_primitive_root(a):
     for p in range(int(log2(a)), 1, -1):
@@ -8,9 +10,9 @@ def get_primitive_root(a):
     return a, 1
 
 terms = set()
-for a in range(2, 101):
+for a in range(lower_bound, upper_bound+1):
     r, p = get_primitive_root(a)
-    for b in range(2, 101):
+    for b in range(lower_bound, upper_bound+1):
         terms.add((r, p * b))
 
 print(len(terms))
