d371ddee5d6e1d0f09ffd19ce8e8c04c3d44b17e
Shageenth Sandrakumar <138735325+shageenthsandrakumar@users.noreply.github.com>
2026-03-13 14:04:24 -0400

    Update solution_1.py

--- problem_030/solution_1.py
+++ problem_030/solution_1.py
@@ -1,10 +1,11 @@
 power = 5
 digits = 1
-upper_bound = digits*(9**power)
+max_sum = digits*(9**power)
 
-while(upper_bound > 10**(digits-1)):
-    upper_bound = digits*(9**power)
+while(max_sum > 10**(digits-1)):
     digits += 1
-
-result = sum(n for n in range(2, upper_bound+ 1) if sum(int(d)**power for d in str(n)) == n)
+    max_sum = digits*(9**power)
+    
+upper_bound = (digits-1)*(9**power)
+result = sum(n for n in range(2, upper_bound+1) if sum(int(d)**power for d in str(n)) == n)
 print(result)
